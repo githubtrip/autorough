4b1d1085c0f77f6e6f8e3b6b0282af743dafe9e9
jacksongs <jacksongsimac@Jacksons-iMac.local>
2016-04-17 17:40:28 +1000

    Added README

---
+++ README.md
@@ -0,0 +1,50 @@
+Why hello, you searingly smart cookie. If you are here for the world's premiere lots-of-videos-in-a-folder-to-Premiere-Pro-rough-cut repo, read on!
+
+Auto Rough
+====
+
+*It's like a Golden Rough, but for exposing your otherwise unseen videos to the world, extremely quickly.*
+
+![A picture of an actual Golden Rough](http://theklutzycook.com/wp-content/uploads/2012/09/Golden-Rough.jpg)
+
+What you need
+----
+
+This is all you need:
+*	Premiere Pro CS6 (it's great, I don't have to pay a subscription for it!),
+*	[This Docker image](https://hub.docker.com/r/jacksongs/autorough/),
+*	Some videos, and
+*	A thumping track in mp3 format.
+
+If you don't use the Docker image, you will need to install into a linux/Mac environment:
+*	ffmpeg,
+*	aubio,
+*	lxml,
+*	python and the PyAV module.
+
+It's a total pain to compile all that crap though. Docker is your overhyped, unpredictable, unpolished but extremely useful friend.
+
+How to get it working
+---
+
+I like to run the Docker image in the current working directory, using this command:
+
+	docker run -it -v `pwd`:`pwd` -w `pwd` jacksongs/autorough
+
+Then, it's as simple as:
+
+	python autorough.py -a <insert the location and name of your audio track here, or just a filename if it's in the current directory > -v <and this is the folder where your videos live>
+
+So, for example, if I had a BuddyHolly.mp3 as my track and my videos lived in /Users/mymac/Documents/Videos, I would type:
+
+	python autorough.py -a BuddyHolly.mp3 -v /Users/jacksongsimac/Documents/Videos 
+
+Got any Docker tips for me? Get in touch, I need as much help as I can get. Just building this goddamn image took an afternoon.
+
+
+The future of Auto Rough
+----
+
+I'm looking to integrate this into a more complete metadata environment and to have it work across Final Cut Pro as well.
+
+Originally I didn't produce an XML, it was just an mp4, but that didn't give me much a chance to tinker with the edit. Could be a good basic option to bring back though.
---
+++ test.py
@@ -0,0 +1,90 @@
+# Good day and welcome! Thanks for visiting this intriguing script.
+# What does it do? I'm glad you asked. It listens to a music track, peers into a folder of videos, and creates a rough cut of the videos matching the beat of the music. Neato!
+
+# First, this part allows you to set where your video files are located, as well as the name and location of your audio track. Note! Your audio file MUST be an mp3.
+import argparse
+
+parser = argparse.ArgumentParser(description='Identify the video and audio folder locations.')
+parser.add_argument('-v','--videofolder',default='/')
+parser.add_argument('-a','--track')
+args = vars(parser.parse_args())
+print args
+
+# Let's save these as nicer variables.
+vidfolder = args['videofolder']
+
+if args['track'] != None:
+	audiofile = args['track'] # Note! Your audio file MUST be an mp3.
+else:
+	audiofile = None
+
+print vidfolder, audiofile
+
+if audiofile != None:
+	# Now here is the magic to get the BPM of the file. This is amended from the aubio repo here: https://github.com/aubio/aubio/blob/master/python/demos/demo_bpm_extract.py
+
+	from aubio import source, tempo
+	from numpy import median, diff
+
+	def get_file_bpm(path, params = {}):
+	    """ Calculate the beats per minute (bpm) of a given file.
+	        path: path to the file
+	        param: dictionary of parameters
+	    """
+	    try:
+	        win_s = params['win_s']
+	        samplerate = params['samplerate']
+	        hop_s = params['hop_s']
+	    except:
+	        """
+	        # super fast
+	        samplerate, win_s, hop_s = 4000, 128, 64 
+	        # fast
+	        samplerate, win_s, hop_s = 8000, 512, 128
+	        """
+	        # default:
+	        samplerate, win_s, hop_s = 44100, 1024, 512
+
+	    s = source(path, samplerate, hop_s)
+	    samplerate = s.samplerate
+	    o = tempo("specdiff", win_s, hop_s, samplerate)
+	    # List of beats, in samples
+	    beats = []
+	    # Total number of frames read
+	    total_frames = 0
+
+	    while True:
+	        samples, read = s()
+	        is_beat = o(samples)
+	        if is_beat:
+	            this_beat = o.get_last_s()
+	            beats.append(this_beat)
+	            #if o.get_confidence() > .2 and len(beats) > 2.:
+	            #    break
+	        total_frames += read
+	        if read < hop_s:
+	            break
+
+	    # Convert to periods and to bpm 
+	    bpms = 60./diff(beats)
+	    b = median(bpms)
+	    return b
+
+	bpm = get_file_bpm(audiofile)
+	beat = bpm/60
+	print 'Beats per second for the audio is',beat
+	print 'Frames per beat (assuming 24fps) is',23.976/beat
+else:
+	beat=2 # This sets a default of 2 beats per second if no audio track is provided.
+
+# This simply gets the duration of the track, using PyAV.
+import av
+fordur = av.open(audiofile)
+print 'Duration is',fordur.duration/1000000.0,'seconds'
+
+
+
+
+
+
+
